9d73ee52fe4ac514866227c5aca537a99749113f
SumonaYeasmin <sumonayeasmin570@gmail.com>
2024-09-24 23:04:27 +0600

    added donation button and history button togol done

--- js/main.js
+++ js/main.js
@@ -1,30 +1,29 @@
 
 const donationBtn = document.getElementById('donation-btn');
 const historyBtn = document.getElementById('history-btn');
+// console.log(donationBtn, historyBtn)
 const donationSection = document.getElementById('donation-section');
 const historySection = document.getElementById('history-container');
+console.log(donationSection, historySection);
 const my_modal= document.getElementById('my_modal');
-console.log(my_modal, historySection);
+// console.log(my_modal, historySection);
 
 // History button show and Donate button Hide
-historyBtn.addEventListener('click', function(event){
-    event.preventDefault();
-    // console.log('click')
+historyBtn.addEventListener('click', function(){
     donationSection.classList.add('hidden');
     historySection.classList.remove('hidden');
-    // donationBtn.classList.add.apply('bg-lime-400', 'text-black');
-    // historyBtn.classList.add.apply('bg-lime-400', 'text-black');
-    
-
-    
+    historyBtn.classList.add('bg-lime-400', 'text-black');
+    donationBtn.classList.remove('bg-lime-400', 'text-black');
+    donationBtn.classList.add('text-gray-500');    
 })
 
 // Donate button show and History button Hide
-donationBtn.addEventListener('click',function(event){
-    event.preventDefault();
-    // console.log('click');
+donationBtn.addEventListener('click',function(){
     donationSection.classList.remove('hidden');
     historySection.classList.add('hidden');
+    donationBtn.classList.add('bg-lime-400', 'text-black');
+    historyBtn.classList.remove('bg-lime-400', 'text-black');
+    donationBtn.classList.remove('text-gray-500');
 })
 
 
@@ -56,12 +55,12 @@ function handleDonateBtn(cardId){
     // Updating the donation amount
     const newCardDonationAmount = cardDonationAmount + donationAmount;
     // console.log(newCardDonationAmount);
-    cardDonationAmountValue.innerText = newCardDonationAmount;
+    cardDonationAmountValue.innerText = newCardDonationAmount.toFixed(2);
 
     // Updating the totalBalance amount
     const newTotalBalance= totalAmount - donationAmount;
     // console.log(newTotalBalance);
-    totalBalance.innerText = newTotalBalance;
+    totalBalance.innerText = newTotalBalance.toFixed(2);
 
     // Clear the input field after donation
     inputDonetionAmount.value = '';
@@ -73,10 +72,7 @@ function handleDonateBtn(cardId){
     const donationTitle = document.getElementById(cardId + '-donate-title').innerText;
     historySection.innerHTML += `
     <div class ="border rounded-xl mx-4 p-5 shadow-inner mb-5">
-        <h3 class = "text-xl font-semibold mb-2">${donationAmount} Taka is ${donationTitle}</h3>
+        <h3 class = "text-xl font-semibold mb-2">${donationAmount.toFixed(2)} Taka is ${donationTitle}</h3>
         <p class = "text-gray-500"> Date: ${new Date()}</p>
     </div>`;
-    
-
-
 }
